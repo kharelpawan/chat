178b102b9e6e0131014883e16c4a613ebe7cb5db
Kharel Pawan <98215488+kharelpawan@users.noreply.github.com>
2025-02-19 12:26:04 +0545

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,28 @@ const wss = new WebSocket.Server({ server });
 
 let users = [];
 let waitingUsers = [];
-let userCount = 0; // For assigning sequential IDs
+let userCount = 0; // Sequential User ID assignment
 
 wss.on('connection', (socket) => {
     userCount++;
-    socket.userId = userCount; // Assign sequential user ID
+    socket.userId = userCount; // Assign unique ID
+    socket.username = null; // Will be assigned by the user
     users.push(socket);
     updateOnlineCount();
 
     socket.on('message', (message) => {
         const data = JSON.parse(message);
 
-        if (data.type === 'start') {
+        if (data.type === 'setName') {
+            socket.username = data.username;
+        } else if (data.type === 'start') {
             findPartner(socket);
         } else if (data.type === 'message' && socket.partner) {
-            socket.partner.send(JSON.stringify({ type: 'message', text: data.text, sender: `User ${socket.userId}` }));
+            socket.partner.send(JSON.stringify({ 
+                type: 'message', 
+                text: data.text, 
+                sender: socket.username || `User ${socket.userId}`
+            }));
         } else if (data.type === 'skip') {
             disconnect(socket, true);
             findPartner(socket);
@@ -39,19 +46,21 @@ wss.on('connection', (socket) => {
 
 function findPartner(socket) {
     if (waitingUsers.length > 0) {
-        // Get a random user from waiting list
-        const randomIndex = Math.floor(Math.random() * waitingUsers.length);
-        const partner = waitingUsers.splice(randomIndex, 1)[0];
+        const partner = waitingUsers.pop();
+        
+        if (partner === socket) {
+            // This should never happen, but just in case
+            socket.send(JSON.stringify({ type: 'error', message: "Can't connect to yourself" }));
+            return;
+        }
 
         socket.partner = partner;
         partner.partner = socket;
 
-        const connectMsg1 = JSON.stringify({ type: 'connected', userId: partner.userId });
-        const connectMsg2 = JSON.stringify({ type: 'connected', userId: socket.userId });
-
-        socket.send(connectMsg1);
-        partner.send(connectMsg2);
+        socket.send(JSON.stringify({ type: 'connected', partnerName: partner.username || `User ${partner.userId}` }));
+        partner.send(JSON.stringify({ type: 'connected', partnerName: socket.username || `User ${socket.userId}` }));
     } else {
+        socket.send(JSON.stringify({ type: 'noUser' }));
         waitingUsers.push(socket);
     }
 }
